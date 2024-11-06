c6f8539021d0290c92d04614035e197859d65218
John Lucky Louis Pilit <j-p7387@students.uc-bcf.edu.ph>
2024-11-06 23:04:15 +0800

    Unfinished Activity

---
+++ app/src/main/java/com/example/mycityapp/DanStuRecoActivity.kt
@@ -0,0 +1,23 @@
+package com.example.mycityapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class DanStuRecoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_dan_stu_reco)
+
+        val navigateButton: ImageView = findViewById(R.id.backButton)
+        navigateButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
